3e6325625a7548701a050fa195245cf1f5134537
nicolajack <nicolacj@bu.edu>
2026-01-31 11:31:14 -0500

    updated unitary2

--- unitary2.py
+++ unitary2.py
@@ -1,15 +1,14 @@
 from qiskit import QuantumCircuit
 from qiskit.qasm3 import dumps as dumps3
+from utils import gates_to_qiskit_circuit
 
-qc = QuantumCircuit(2)
-qc.t(1)
-qc.t(1)
-qc.cx(0, 1)
-qc.tdg(1)
-qc.tdg(1)
+input_seq = "HTSHTSHTHTSHTSHTHTHTSHTHTSHTSHTHTSHTHTHTSHTHTHTHTSHTSHTSHTHTSHTSHTHTHTHTSHTSHTSHTSHTSHTHTSHTHTHTSHTSHTSHTHTSHTHTHTSHTSHTHTHTSHTSHTSHTSHTHTHTHTHTSHTSHTHTHTSHTHTSHTSHTSHTSHTSHTHTHTHTHTSHTHTHTSHTHTHTHTSHTSHTSHTHTSHTSHTSHTSHTSHTSHTHTSHTSHTSHTHTHTSHTSHTSHTSHTSHTSHXSS"
 
-print(qc.draw())
+qc = gates_to_qiskit_circuit(input_seq, i=1, reverse=False)
 
+# Export to QASM3
 qasm3_str = dumps3(qc)
 with open("unitary2.qasm", 'w') as file:
     file.write(qasm3_str)
+
+print("\nQASM3 file saved as unitary2.qasm")
