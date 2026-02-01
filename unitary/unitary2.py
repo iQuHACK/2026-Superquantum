e385dc15c752fe480974ccd346fcaec60ddf0c06
Adam Godel <agodel@bu.edu>
2026-02-01 01:16:43 -0500

    Paper draft + change Rx to Ry in unitary 2

--- unitary/unitary2.py
+++ unitary/unitary2.py
@@ -1,15 +1,15 @@
 from qiskit import QuantumCircuit
 from qiskit.qasm3 import dumps as dumps3
-from utils import Rz
+from utils import Ry
 import math
 
 theta = math.pi / 7
 epsilon = 1.0e-01
 
 qc = QuantumCircuit(2)
-qc.append(Rz(theta/2, epsilon).to_gate(), [1])
+qc.append(Ry(theta/2, epsilon).to_gate(), [1])
 qc.cx(0, 1)
-qc.append(Rz(-theta/2, epsilon).to_gate(), [1])
+qc.append(Ry(-theta/2, epsilon).to_gate(), [1])
 qc.cx(0, 1)
 
 qasm3_str = dumps3(qc)
